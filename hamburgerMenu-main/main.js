df25176b41abab04a44a87764a9da06878bdb787
kazukimmm <brooklynnets_1192@icloud.com>
2025-03-05 10:36:30 +0900

    Initial commit

---
+++ hamburgerMenu-main/main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// {
+//     const hamburger = document.querySelector(".hamburger");
+//     const nav = document.querySelector(".nav");
+
+//     hamburger.addEventListener("click", () => {
+//         hamburger.classList.toggle("open");
+//         nav.classList.toggle("open");
+//     });
+// }
+
+var carousel = $(".carousel"),
+    currdeg  = 0;
+
+$(".next").on("click", { d: "n" }, rotate);
+$(".prev").on("click", { d: "p" }, rotate);
+
+function rotate(e){
+  if(e.data.d=="n"){
+    currdeg = currdeg - 60;
+  }
+  if(e.data.d=="p"){
+    currdeg = currdeg + 60;
+  }
+  carousel.css({
+    "-webkit-transform": "rotateY("+currdeg+"deg)",
+    "-moz-transform": "rotateY("+currdeg+"deg)",
+    "-o-transform": "rotateY("+currdeg+"deg)",
+    "transform": "rotateY("+currdeg+"deg)"
+  });
+}
